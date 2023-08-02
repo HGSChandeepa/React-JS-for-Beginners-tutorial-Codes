4e4f7a593cdf1ac720e86ca8dd502e81d8593520
samin <84340577+HGSChandeepa@users.noreply.github.com>
2023-08-03 00:18:44 +0530

    PROPS

--- src/App.js
+++ src/App.js
@@ -3,34 +3,42 @@ import "./App.css";
 function App() {
   return (
     <div>
-      <UserData />
-      <UserLink />
+      <UserData userEmail="jhondoe@gmail.com" userName="Jhon Doe" />
+      <UserLink
+        title="link-1"
+        href="https://www.linkedin.com/company/login/ "
+      />
 
-      <UserData />
-      <UserLink />
+      <UserData userName="Saman Kumara" userEmail="samansaman@gmail.com" />
+      <UserLink title="link-2" href="https://github.com/" />
 
-      <UserData />
-      <UserLink />
+      <UserData userName="Maranun Maris" userEmail="marunmaris@gmail.com" />
+      <UserLink
+        title="link-3"
+        href="https://one-tender.atlassian.net/jira/your-work"
+      />
     </div>
   );
 }
 
 //userData component
-const UserData = () => {
+const UserData = (props) => {
   return (
     <div>
-      <h2>John Doe</h2>
-      <div>useremail@gmail.com</div>
+      <h2>{props.userName}</h2>
+      <div>{props.userEmail}</div>
     </div>
   );
 };
 
 //userLink component
 
-const UserLink = () => {
+const UserLink = (props) => {
   return (
     <div>
-      <a href="https://ejs.co/">click me</a>
+      <a href={props.href} target="_blank">
+        {props.title}
+      </a>
     </div>
   );
 };
